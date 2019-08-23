eda4583cac4f105300444578a0528664e107882e
Brian Illes <brianrilles@gmail.com>
2019-08-23 09:59:05 -0700

    Adds routes and playlist controller

---
+++ app/controllers/api/v1/playlist_controller.rb
@@ -0,0 +1,29 @@
+module Api
+    module V1
+        class PlaylistController < ApplicationController
+            # before actions
+
+            # should only work if nested under user
+            def index
+                @playlists = Playlist.find(user_id: params[:user_id])
+
+                render json: @playlists
+            end
+
+            # should only work withour nesting
+            def show
+                @playlist = Playlist.find(params[:id])
+
+                render json: @playlist
+            end
+
+            # should only work if nested under a user
+            def create
+                @playlist = Playlist.new(playlist_params)
+                @playlist.save! # throw an exception
+
+                render json: @playlist
+            end
+        end
+    end
+end
